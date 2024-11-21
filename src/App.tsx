4eeec827207462a2000c617ca89e6808a0715e99
qedk <1994constant@gmail.com>
2024-11-21 14:28:38 +0700

    feat: implement stavail bridging

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import WormholeConnect, {
   } from '@wormhole-foundation/wormhole-connect';
   
   const wormholeConfig: WormholeConnectConfig = {
-    network: 'Testnet',
-    chains: ['ArbitrumSepolia', 'Solana'],
-    tokens: ['WSVarbsep', 'WSVsol'],
+    network: 'Mainnet',
+    chains: ['Ethereum', 'Base'],
+    tokens: ['StakedAvail', 'StakedAvailBase'],
     ui: {
       title: 'Wormhole NTT UI',
       defaultInputs: {
-        fromChain: 'ArbitrumSepolia',
-        toChain: 'Solana'
+        fromChain: 'Ethereum',
+        toChain: 'Base'
       },
       showHamburgerMenu: false,
     },
@@ -19,24 +19,46 @@ import WormholeConnect, {
       ...nttRoutes({
         tokens: {
           WSV_NTT: [
+            // {
+            //   chain: 'Ethereum',
+            //   manager: '0x2E65520ff593b583A2e5895174eF7F40F78a90BD',
+            //   token: '0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8',
+            //   transceiver: [
+            //     {
+            //       address: '0xFaD96a1B1dad4a4391EEAb773Df739032526f389',
+            //       type: 'wormhole',
+            //     },
+            //   ],
+            // },
             {
-              chain: 'ArbitrumSepolia',
-              manager: '0x3F52328B390276eFF9C77940Bc7F81e098De5Ed1',
-              token: '0xff1Fa5B426C6155fbc7e22da6700Ad8C95Da01F4',
+              chain: 'Ethereum',
+              manager: '0x71C4259648E5e6502C3cd29fB9aa818EF0142DD2',
+              token: '0x3742f3Fcc56B2d46c7B8CA77c23be60Cd43Ca80a',
               transceiver: [
                 {
-                  address: '0xA12b94F1a82fbd3bD2721659e692A6467b3Fe478',
+                  address: '0xb61306eCA0284aa4FcD09C1f40f7824A59824697',
                   type: 'wormhole',
                 },
               ],
             },
+            // {
+            //   chain: 'Base',
+            //   manager: '0x4b3d190ca333a1414376Dd565ACBa58350A36d67',
+            //   token: '0xd89d90d26B48940FA8F58385Fe84625d468E057a',
+            //   transceiver: [
+            //     {
+            //       address: '0xc986dD854B8F4acb5ccF2fa1cdacC113e6de0892',
+            //       type: 'wormhole',
+            //     },
+            //   ],
+            // },
             {
-              chain: 'Solana',
-              manager: 'ntNGLGC45T7X1cMX6ezdPdcZDUwEQL3sb62nhEVhLwa',
-              token: 'Hyfw9cTZbMaWJqBcWxutVkT8NLuCwixbdGj6zWY7amD2',
+              chain: 'Base',
+              manager: '0x931c9E3a44A48F5b80D7B4aBB25E28AB12D1Ad2A',
+              token: '0x74cb668d23E6e54524e2E1e4d1c392F5fd611783',
               transceiver: [
                 {
-                  address: '2PeCzGTK2j4cMSb2yZQxLfVBbnsuenf7i3KdSTRfPRZi',
+                  address: '0x2C1812D91086Cd5538F408Ed47C813780a3F5583',
                   type: 'wormhole',
                 },
               ],
@@ -46,32 +68,58 @@ import WormholeConnect, {
       }),
     ],
     tokensConfig: {
-      WSVarbsep: {
-        key: 'WSVarbsep',
-        symbol: 'WSV',
-        nativeChain: 'ArbitrumSepolia',
-        displayName: 'WSV',
+      // Avail: {
+      //   key: 'Avail',
+      //   symbol: 'AVAIL',
+      //   nativeChain: 'Ethereum',
+      //   displayName: 'AVAIL',
+      //   tokenId: {
+      //     chain: 'Ethereum',
+      //     address: '0xEeB4d8400AEefafC1B2953e0094134A887C76Bd8'
+      //   },
+      //   coinGeckoId: 'avail',
+      //   icon: 'https://wormhole.com/token.png',
+      //   decimals: 18
+      // },
+      StakedAvail: {
+        key: 'StakedAvail',
+        symbol: 'stAVAIL',
+        nativeChain: 'Ethereum',
+        displayName: 'stAVAIL',
         tokenId: {
-          chain: 'ArbitrumSepolia',
-          address: '0xff1Fa5B426C6155fbc7e22da6700Ad8C95Da01F4'
+          chain: 'Ethereum',
+          address: '0x3742f3Fcc56B2d46c7B8CA77c23be60Cd43Ca80a'
         },
-        coinGeckoId: 'wormhole',
+        coinGeckoId: 'staked-avail',
         icon: 'https://wormhole.com/token.png',
         decimals: 18
       },
   
-      WSVsol: {
-        key: 'WSVsol',
-        symbol: 'WSV',
-        nativeChain: 'Solana',
-        displayName: 'WSV',
+      // AvailBase: {
+      //   key: 'AvailBase',
+      //   symbol: 'AVAIL',
+      //   nativeChain: 'Base',
+      //   displayName: 'AVAIL',
+      //   tokenId: {
+      //     chain: 'Base',
+      //     address: '0xd89d90d26B48940FA8F58385Fe84625d468E057a'
+      //   },
+      //   coinGeckoId: 'avail',
+      //   icon: 'https://wormhole.com/token.png',
+      //   decimals: 18
+      // },
+      StakedAvailBase: {
+        key: 'StakedAvailBase',
+        symbol: 'stAVAIL.W',
+        nativeChain: 'Base',
+        displayName: 'stAVAIL.W',
         tokenId: {
-          chain: 'Solana',
-          address: 'Hyfw9cTZbMaWJqBcWxutVkT8NLuCwixbdGj6zWY7amD2'
+          chain: 'Base',
+          address: '0x74cb668d23E6e54524e2E1e4d1c392F5fd611783'
         },
-        coinGeckoId: 'wormhole',
+        coinGeckoId: 'staked-avail',
         icon: 'https://wormhole.com/token.png',
-        decimals: 9
+        decimals: 18
       }
     }
   }
